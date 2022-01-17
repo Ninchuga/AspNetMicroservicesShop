6f4a3c8a63bc92c391918d2e38e352cdfa026fd8
Ninoslav DJUKIC <ninoslav90@hotmail.com>
2022-01-18 00:18:40 +0100

    Saga Orchestrator, Payment API, Delivery API and order saga db image added to docker compose file

--- Src/Orchestrators/Shopping.CheckoutOrchestrator/Startup.cs
+++ Src/Orchestrators/Shopping.CheckoutOrchestrator/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Shopping.HealthChecks;
 using Shopping.OrderSagaOrchestrator.Extensions;
 using Shopping.OrderSagaOrchestrator.Services;
 using System;
---
+++ Src/Services/Payment/Payment.API/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# exit when any command fails
+set -e
+
+# trust dev root CA
+openssl x509 -inform DER -in /https-root/shopping-root-cert.cer -out /https-root/shopping-root-cert.crt
+cp /https-root/shopping-root-cert.crt /usr/local/share/ca-certificates/
+chmod 644 /usr/local/share/ca-certificates/
+update-ca-certificates
+
+# start the app
+dotnet Payment.API.dll
