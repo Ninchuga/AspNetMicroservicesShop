3dbff7b469e56f795977d4e5946f83b05caae6da
Ninoslav DJUKIC <ninoslav90@hotmail.com>
2022-02-07 23:47:04 +0100

    Update acr task yaml

--- Src/create_azure_kubernetes.ps1
+++ Src/create_azure_kubernetes.ps1
@@ -97,7 +97,7 @@ az keyvault secret set `
 az acr task create `
     --registry $ACR_NAME `
     --name webrazor-build-task `
-    --context https://github.com/$GIT_USER/AspNetMicroservicesShop.git#main:src `
+    --context https://github.com/$GIT_USER/AspNetMicroservicesShop.git#main `
     --file Src/acrmultitask.yaml `
     --git-access-token $(az keyvault secret show --vault-name $AKV_NAME --name $ACR_NAME-git-acr-task-token --query value -o tsv)
 
