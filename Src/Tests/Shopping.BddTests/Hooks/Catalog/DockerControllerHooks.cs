36277bd9db694c2a41b14589cf6d1fecc2a42290
Ninoslav DJUKIC <ninoslav90@hotmail.com>
2022-10-03 23:14:46 +0200

    BDD Tests specflow initial implementation...not working

---
+++ Src/Tests/Shopping.BddTests/Hooks/Catalog/DockerControllerHooks.cs
@@ -0,0 +1,87 @@
+﻿using Ductus.FluentDocker.Services;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using Ductus.FluentDocker.Builders;
+using System.Net;
+using BoDi;
+using System.Net.Http;
+
+namespace Shopping.BddTests.Hooks.Catalog
+{
+    [Binding]
+    public class DockerControllerHooks
+    {
+        private static ICompositeService _compositeService;
+        private IObjectContainer _objectCotainer; // DI container in SpecFlow. With it we can add dependencies in the Steps
+
+        public DockerControllerHooks(IObjectContainer objectCotainer)
+        {
+            _objectCotainer = objectCotainer;
+        }
+
+        [BeforeTestRun]
+        public static void DockerComposeUp()
+        {
+            var config = LoadConfiguration();
+
+            var dockerComposeFileName = config["DockerComposeFileName"];
+            var dockerComposePath = GetDockerComposeLocation(dockerComposeFileName);
+
+            var confirmationUrl = config["CatalogApiUrl"];
+            _compositeService = new Builder()
+                .UseContainer()
+                .UseCompose()
+                .FromFile(dockerComposePath)
+                .RemoveOrphans()
+                .ForceRecreate()
+                //.WaitForHttp("catalog.api", $"{confirmationUrl}/api/v1/Catalog",
+                //    continuation: (response, _) => response.Code != HttpStatusCode.OK ? 2000 : 0)
+                .Build()
+                .Start();
+        }
+
+        [AfterTestRun]
+        public static void DockerComposeDown()
+        {
+            _compositeService.Stop();
+            _compositeService.Dispose();
+        }
+
+        [BeforeScenario()]
+        public void AddHttpClient()
+        {
+            var config = LoadConfiguration();
+            var httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(config["CatalogApiUrl"])
+            };
+            _objectCotainer.RegisterInstanceAs(httpClient);
+        }
+
+        private static IConfiguration LoadConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+        }
+
+        private static string GetDockerComposeLocation(string dockerComposeFileName)
+        {
+            var directory = Directory.GetCurrentDirectory();
+            while (!Directory.EnumerateFiles(directory, "*.yml").Any(s => s.EndsWith(dockerComposeFileName)))
+            {
+                directory = directory.Substring(0, directory.LastIndexOf(Path.DirectorySeparatorChar));
+            }
+
+            return Path.Combine(directory, dockerComposeFileName);
+
+        }
+    }
+}
---
+++ Src/Tests/Shopping.BddTests/Steps/Catalog/LoggedInUsersCanSeeListOfProductsInCatalogSteps.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using Shopping.BddTests.Models.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Shopping.BddTests.Steps.Catalog
+{
+    [Binding]
+    public class LoggedInUsersCanSeeListOfProductsInCatalogSteps
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ScenarioContext _scenarioContext; // put an items in this context and extract them in other steps
+
+        public LoggedInUsersCanSeeListOfProductsInCatalogSteps(HttpClient httpClient, ScenarioContext scenarioContext)
+        {
+            _httpClient = httpClient;
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"a user that is not logged in")]
+        public void GivenAUserThatIsNotLoggedIn()
+        {
+            //ScenarioContext.StepIsPending();
+        }
+
+        [When(@"tries to see catalog")]
+        public async Task WhenTriesToSeeCatalog()
+        {
+            //ScenarioContext.StepIsPending();
+            var response = await _httpClient.GetAsync("/api/v1/Catalog");
+            //var responseCatalog = await response.Content.ReadFromJsonAsync<IEnumerable<Product>>();
+            _scenarioContext.Add("CatalogUnauthorizedResponse", response);
+            //_scenarioContext.Add("CatalogProducts", responseCatalog);
+        }
+
+        [Then(@"unathorized response is returned")]
+        public void ThenUnathorizedResponseIsReturned()
+        {
+            //ScenarioContext.StepIsPending();
+            //var catalog = _scenarioContext.Get<IEnumerable<Product>>("Catalog");
+            var response = _scenarioContext.Get<HttpResponseMessage>("CatalogUnauthorizedResponse");
+            //var catalogProducts = await response.Content.ReadFromJsonAsync<IEnumerable<Product>>();
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+            response.Content.Should().BeNull();
+
+        }
+
+        [Given(@"a logged in user")]
+        public void GivenALoggedInUser()
+        {
+            ScenarioContext.StepIsPending();
+        }
+        
+        [When(@"tries to get catalog")]
+        public void WhenUserTriesToGetCatalog()
+        {
+            ScenarioContext.StepIsPending();
+        }
+        
+        [Then(@"catalog with products is displayed")]
+        public void ThenCatalogWithProductsIsDisplayed()
+        {
+            ScenarioContext.StepIsPending();
+        }
+    }
+}
